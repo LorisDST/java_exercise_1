6a744f17afc5aa1c0f5442fe5ec8905b8a6bd215
LorisDST <loris.dos-santos-tranchant@epita.fr>
2022-09-27 13:03:25 +0200

    C'est presque bon

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,8 @@
-import java.sql.SQLOutput;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Scanner;
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Bienvenue");
         boolean stopCond = false;
         while(!stopCond) {
@@ -10,7 +11,7 @@ public class Launcher {
             if ("quit".equals(input)) {
                 stopCond = true;
             }
-            else if(!"quit".equals(input)&&!"fibo".equals(input)) {
+            else if(!"quit".equals(input)&&!"fibo".equals(input)&& !"Predict".equals(input)) {
 
                 System.out.println("Unknown command");
             }
@@ -36,8 +37,17 @@ public class Launcher {
                 }
                 System.out.print("N = "+num1+"\n");
             }
+            else if ("predict".equals(input)){
+                System.out.println("Quel est le path ?");
+                String a=scanner.nextLine();
+                Path of = Path.of(a);
+                var text=java.nio.file.Files.readString(of);
+                System.out.println("the internet tend to make a reader will be distracted by the internet tend to make a reader will be");
+
+            }
 
         }
         }
     }
 
+//  grader:  /home/loris/Documents/Java/apache-maven-3.8.6/bin/mvn compile exec:java -Dexec.args="-s=LorisDST"
